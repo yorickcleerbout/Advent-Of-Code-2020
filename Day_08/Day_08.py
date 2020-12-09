5ae165c3fc90d9dcd8351c1000e5ebc06ab69494
Yorick Cleerbout <70236777+yorickcleerbout@users.noreply.github.com>
2020-12-09 11:40:24 +0100

    Finished Part Two!

--- Day_08/Day_08.py
+++ Day_08/Day_08.py
@@ -1,43 +1,99 @@
-with open('Day_08/input.txt', 'r') as f:
-    data = f.readlines()
+import pandas as pd
 
-with open('Day_08/test.txt', 'r') as f:
-    test = f.readlines()
+f = open("Day_08/input.txt", "r")
+inputs = f.read()
+
+codes = [x for x in inputs.strip("\n").split("\n")]
 
 
-def part_one(data):
-    accumulator = 0
-    index = 0
-    visited = []
-    while index < len(data):
-        elem = data[index].split(' ')
-        action = elem[0]
-        value = elem[1].replace("\n", "")
+def createCodes():
+    cleanCodes = []
+    for code in codes:
+        splits = code.split(" ")
+        command = splits[0]
+        value = int(splits[1])
+        dictCode = {"code": command, "value": value, "hasRun": 0}
+        cleanCodes.append(dictCode)
+    return cleanCodes
 
-        if index not in visited:
-            visited.append(index)
-        else:
-            return accumulator
 
-        if action == "acc":
-            if value[:1] == "+":
-                accumulator += int(value[1:])
-                index += 1
-            else:
-                accumulator -= int(value[1:])
-                index += 1
-        elif action == "jmp":
-            if value[:1] == "+":
-                index += int(value[1:])
-            else:
-                index -= int(value[1:])
-        elif action == "nop":
-            index += 1
+# PART 1 MATERIAL
+def part_one():
+    dictCodes = createCodes()
+
+    stopInfLoop = 0
+    lineRun = 0
+    accVal = 0
+    while stopInfLoop == 0:
+
+        if dictCodes[lineRun]['hasRun'] == 1:
+            stopInfLoop = 1
+            break
+
+        code = dictCodes[lineRun]['code']
+        val = dictCodes[lineRun]['value']
+        dictCodes[lineRun]['hasRun'] = 1
+
+        if code == "nop":
+            lineRun += 1
+        elif code == "acc":
+            accVal += val
+            lineRun += 1
+        elif code == "jmp":
+            lineRun += val
+    return accVal
+
+# PART 2 MATERIAL
 
 
 def part_two():
-    return "Did not finish this part."
+    dictCodes = createCodes()
+
+    df = pd.DataFrame(dictCodes)
+
+    nops = df[df['code'] == 'nop']
+    jmps = df[df['code'] == 'jmp']
+    nopsLines = list(nops.index)
+    jmpsLines = list(jmps.index)
+    checkLines = jmpsLines.extend(nopsLines)
+
+    def resetHasRun(codes):
+        for code in codes:
+            code['hasRun'] = 0
+
+    dictCodes = createCodes()
+    foundFix = 0
+    for checkLine in jmpsLines:
+        if foundFix == 1:
+            break
+
+        dictCodes[checkLine]['code'] = 'nop'
+        stopInfLoop = 0
+        lineRun = 0
+        accVal = 0
+
+        while stopInfLoop == 0 and foundFix == 0:
+            if dictCodes[lineRun]['hasRun'] == 1:
+                stopInfLoop = 1
+                dictCodes[checkLine]['code'] = 'jmp'
+                break
+
+            code = dictCodes[lineRun]['code']
+            val = dictCodes[lineRun]['value']
+            dictCodes[lineRun]['hasRun'] = 1
+
+            if code == "nop":
+                lineRun += 1
+            elif code == "acc":
+                accVal += val
+                lineRun += 1
+            elif code == "jmp":
+                lineRun += val
+            if lineRun == len(codes):
+                foundFix = 1
+                return accVal
+        resetHasRun(dictCodes)
 
 
-print("Immediately before any instruction is executed a second time, what value is in the accumulator? (Part 1):", part_one(data))
-print(part_two())
+print("What value is in the accumulator? (Part 1):", part_one())
+print("What is the value of the accumulator after the program terminates? (Part 2):", part_two())
